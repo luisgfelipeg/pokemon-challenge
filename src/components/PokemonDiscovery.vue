4fda7782ad8ebb51f120127958122c5b4633b731
Luis Felipe <lfelipeggarcia@gmail.com>
2023-02-11 08:57:17 -0300

    feat: error msg

--- src/components/PokemonDiscovery.vue
+++ src/components/PokemonDiscovery.vue
@@ -9,6 +9,7 @@ export default {
     return {
       pokemon: '',
       findPokemon: true,
+      notFound: false,
     };
   },
   methods: {
@@ -16,7 +17,9 @@ export default {
       const pokemonStore = usePokemonStore();
 
       const name = await (
-        await Api.get(`pokemon/${this.pokemon.toLowerCase()}`)
+        await Api.get(`pokemon/${this.pokemon.toLowerCase()}`).catch(
+          (err) => (this.notFound = true)
+        )
       ).data.name;
 
       const species = await (
@@ -86,27 +89,41 @@ export default {
 </script>
 
 <template>
-  <template v-if="findPokemon"
-    ><form v-on:submit.prevent="getPokemon">
-      <input
-        type="text"
-        placeholder="Nome do pokémon"
-        class="search__input"
-        v-model="pokemon"
-      />
-      <button class="search__button">
-        <svg class="search__icon" aria-hidden="true" viewBox="0 0 24 24">
-          <g>
-            <path
-              d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
-            ></path>
-          </g>
-        </svg>
-      </button></form
+  <template v-if="findPokemon">
+    <div class="firstDiv">
+      <strong v-if="notFound"
+        >Pokémon nao encontrado, digite um pokémon valido!</strong
+      >
+      <form v-on:submit.prevent="getPokemon">
+        <input
+          type="text"
+          placeholder="Nome do pokémon"
+          class="search__input"
+          v-model="pokemon"
+        />
+        <button class="search__button">
+          <svg class="search__icon" aria-hidden="true" viewBox="0 0 24 24">
+            <g>
+              <path
+                d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
+              ></path>
+            </g>
+          </svg>
+        </button>
+      </form></div
   ></template>
 </template>
 
 <style lang="scss" scoped>
+.firstDiv {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  strong {
+    color: black;
+  }
+}
 .search {
   display: flex;
   align-items: center;
